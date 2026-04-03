14823f501058b528e910936cafb2641cb609ad5b
yamabiko-kouhou <yamabiko.nagatutakouhou@gmail.com>
2026-04-03 10:04:36 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -119,35 +119,6 @@ const swiper = new Swiper('.swiper', {
 
 
 
-//タブメニュー(MRI・CTページ)
-function GethashID (hashIDName){
-	if(hashIDName){
-		$('.tab li').find('a').each(function() { 
-			var idName = $(this).attr('href'); 
-			if(idName == hashIDName){ 
-				var parentElm = $(this).parent();
-				$('.tab li').removeClass("active");
-				$(parentElm).addClass("active");
-				$(".tab-area").removeClass("is-active");
-				$(hashIDName).addClass("is-active");
-			}
-		});
-	}
-}
-$('.tab a').on('click', function() {
-	var idName = $(this).attr('href');
-	GethashID (idName);
-	return false;
-});
-$(window).on('load', function () {
-    $('.tab li:first-of-type').addClass("active");
-    $('.tab-area:first-of-type').addClass("is-active");
-	var hashName = location.hash;
-	GethashID (hashName);
-});
-
-
-
 // スクロールした時ふわっと出現するエフェクト
 $('.effect').on('inview', function() {
 	 var $item = $(this);
